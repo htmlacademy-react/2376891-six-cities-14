f76edb0e4688dc28dca2a64d86bb6a5d312c04a8
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2023-12-04 14:55:25 +0300

    Adds a component for rendering an empty main page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -2,14 +2,22 @@ import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
 import Cities from '../../components/cities/cities';
 import CityNamesList from '../../components/city-names-list/city-names-list';
+import NoCities from '../../components/no-cities/no-cities';
 import { useAppSelector } from '../../hooks';
 import { getActiveCity } from '../../store/app-process/selectors';
 import { getOffers } from '../../store/data-process/selectors';
+import { TOffers } from '../../types/offer';
 
 function MainPage(): JSX.Element | null {
   const activeCity = useAppSelector(getActiveCity);
   const offers = useAppSelector(getOffers);
-  const offersByCity = offers.filter((offer) => offer.city.name === activeCity);
+  const isNoOffers = (offers.length === 0);
+
+  let offersByCity: TOffers = [];
+
+  if (!isNoOffers) {
+    offersByCity = offers.filter((offer) => offer.city.name === activeCity);
+  }
 
   if (offers.length === 0) {
     return null;
@@ -21,10 +29,14 @@ function MainPage(): JSX.Element | null {
         <title>{'6 cities'}</title>
       </Helmet>
       <Header />
-      <main className="page__main page__main--index">
+      <main className={`page__main page__main--index ${isNoOffers ? 'page__main--index-empty' : ''}`}>
         <h1 className="visually-hidden">Cities</h1>
         <CityNamesList />
-        <Cities offersByCity={offersByCity} activeCity={activeCity} ></Cities>
+        {isNoOffers ? (
+          <NoCities activeCity={activeCity} />
+        ) : (
+          <Cities offersByCity={offersByCity} activeCity={activeCity} ></Cities>
+        )}
       </main>
     </div>
   );
